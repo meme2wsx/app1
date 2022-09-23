07189ae86b54220d2108338c9e0d9a35534d2f85
meme2wsx <meme2wsx@gmail.com>
2022-09-24 01:43:15 +0900

    スタイルを変更する

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-const html = `<h2><a id="title">ようこそ</a></h2><p>typescriptが出力しています</p>`
+const html = `<h2><a id="title">ようこそ</a></h2><div id="content">typescriptが出力しています</div>`
 
 window.addEventListener('load', () => {
     let msg: HTMLParagraphElement = document.querySelector('#msg')
@@ -7,4 +7,15 @@ window.addEventListener('load', () => {
     const title: HTMLAnchorElement = document.querySelector('#title')
     title.href = "http://localhost:8080"
     
+    const content:HTMLDivElement = document.querySelector('#content')
+    setDiv(content)
+
+    function setDiv(content:HTMLDivElement) {
+        content.style.width = "300px"
+        content.style.height ="300px"
+        content.style.borderWidth = "3px"
+        content.style.borderStyle = "solid"
+        content.style.backgroundColor = "yellowgreen"
+        content.textContent = "setDiv関数が上書きしました"
+    }
 })
