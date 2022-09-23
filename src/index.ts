182c2369c07a5fbe8180a6837e610e1a70eaf1a5
meme2wsx <meme2wsx@gmail.com>
2022-09-24 01:21:54 +0900

    ちょっと整理整頓しただけ

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-let msg:HTMLParagraphElement
-
 const html = `<h2><a id="title">ようこそ</a></h2><p>typescriptが出力しています</p>`
 
 window.addEventListener('load', () => {
-    msg = document.querySelector('#msg')
+    let msg: HTMLParagraphElement = document.querySelector('#msg')
     msg.innerHTML = html
-    const title:HTMLAnchorElement = document.querySelector('#title')
+
+    const title: HTMLAnchorElement = document.querySelector('#title')
     title.href = "http://localhost:8080"
+    
 })
