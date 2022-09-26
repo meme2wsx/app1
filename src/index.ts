5c8dd70f1c4999d4b9d2c88deb85405c927e8fed
meme2wsx <meme2wsx@gmail.com>
2022-09-27 07:14:08 +0900

    マウスクリックイベントで処理を実行させる

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,37 @@
-const html = `<h2><a id="title">ようこそ</a></h2><div id="content">typescriptが出力しています</div>`
+let msg: HTMLParagraphElement
+
+const html = `<h3>赤枠内にパンダの絵を描きなさい。</h3><div id="content">wait...</div>`
 
 window.addEventListener('load', () => {
-    let msg: HTMLParagraphElement = document.querySelector('#msg')
+    msg = document.querySelector('#msg')
     msg.innerHTML = html
-
-    const title: HTMLAnchorElement = document.querySelector('#title')
-    title.href = "http://localhost:8080"
-    
-    const content:HTMLDivElement = document.querySelector('#content')
+    const content: HTMLDivElement = document.querySelector('#content')
     setDiv(content)
-    addElement1(content)
-    addElement2(content)
-
-    function setDiv(content:HTMLDivElement) {
-        content.style.position = "absolute"
-        content.style.left = "100px"
-        content.style.top = "300px"
-        content.style.width = "300px"
-        content.style.height ="300px"
-        content.style.borderWidth = "3px"
-        content.style.borderStyle = "solid"
-        content.style.backgroundColor = "yellowgreen"
-        content.textContent = "setDiv関数が上書きしました"
-        content.style.borderColor = "blue"
-    }
 })
 
-function addElement1(content: HTMLDivElement) {
-    let div: HTMLDivElement = document.createElement('div')
+function addElement(event: MouseEvent) {
+    const div: HTMLDivElement = document.createElement('div')
     div.style.position = "absolute"
-    div.style.width = "50px"
-    div.style.height = "50px"
-    div.style.backgroundColor = "red"
-    div.style.top = "40px"
-    div.textContent = "add1"
-    content.appendChild(div)
-
+    div.style.width = "25px"
+    div.style.height = "25px"
+    div.style.left = event.offsetX  + "px"
+    div.style.top = event.offsetY  + "px"
+    div.style.backgroundColor = "#000000"
+    const target: HTMLElement = event.target as HTMLElement
+    target.appendChild(div)
 }
 
-function addElement2(content: HTMLDivElement) {
-    let div: HTMLDivElement = document.createElement('div')
-    div.style.position = "absolute"
-    div.style.width = "50px"
-    div.style.height = "50px"
-    div.style.backgroundColor = "green"
-    div.style.top = "100px"
-    div.textContent = "add2"
-    content.appendChild(div)
+function setDiv(content: HTMLDivElement) {
+    content.style.position = "absolute"
+    content.style.left = "100px"
+    content.style.top ="100px"
+    content.style.width = "300px"
+    content.style.height = "300px"
+    content.style.borderWidth = "3px"
+    content.style.borderStyle = "solid"
+    content.style.borderColor = "red"
+    content.style.backgroundColor = "white"
+    content.textContent = ""
+    content.addEventListener('click', addElement)
 }
+
